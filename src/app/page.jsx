fd5661f2a8087b0abebc5f378f8e652f99444c79
hanocksuresh <92392566+hanocksuresh@users.noreply.github.com>
2026-07-28 12:48:03 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -46,7 +46,6 @@ export default function HomePage() {
           delay: 1,
         }}
         className="fixed bottom-4 right-4 text-sm text-white/40 pointer-events-none z-50 font-light">
-        @anujbuilds
       </motion.div>
     </main>
   )
